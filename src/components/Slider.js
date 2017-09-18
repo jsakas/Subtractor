340209abeede2e27c4f53427c91bd4c653cb4671
Jon Sakas <jon.sakas@beatport.com>
2017-09-17 18:28:35 -0600

    Add label and value container, update DOM when input is changed

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,24 +6,56 @@ class Slider extends HTMLElement {
   connectedCallback() {
     const shadow = this.attachShadow({ mode: 'open' })
 
+    this.id = this.getAttribute('id')
     this.name = this.getAttribute('name')
     this.min = this.getAttribute('min')
     this.max = this.getAttribute('max')
     this.value = this.getAttribute('value')
-    this.fn = new Function(this.getAttribute('fn'))
+    this.oninput = new Function('value', this.getAttribute('oninput'))
 
-    const range = document.createElement('input')
-    range.type = 'range'
-    range.min = this.min
-    range.max = this.max
-    range.value = this.value
+    this.div = document.createElement('div')
 
-    range.addEventListener('input', (e) => { 
+    this.label = document.createElement('label')
+    this.label.for = this.id
+    this.label.innerText = this.name
+
+    this.range = document.createElement('input')
+    this.range.type = 'range'
+    this.range.id = this.id
+    this.range.min = this.min
+    this.range.max = this.max
+    this.range.value = this.value
+    this.range.addEventListener('input', (e) => { 
+      this.setAttribute('value', e.target.value)
       this.value = e.target.value
-      this.fn()
+      this.oninput(this.value)
     })
-    shadow.appendChild(range)
+
+    this.span = document.createElement('span')
+    this.span.innerText = this.value
+
+    this.div.appendChild(this.label)
+    this.div.appendChild(this.range)
+    this.div.appendChild(this.span)
+    shadow.appendChild(this.div)
   }
+
+  static get observedAttributes() {
+    return [
+      'id',
+      'name',
+      'min',
+      'max',
+      'value'
+    ]
+  }
+
+  attributeChangedCallback(attr, oldValue, newValue) {
+    if (attr == 'value' && this.span) {
+      this.span.innerText = newValue
+    }
+  }
+
 }
 
 window.customElements.define('x-slider', Slider)
