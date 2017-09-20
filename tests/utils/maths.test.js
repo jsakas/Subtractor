13bf9e318b8e4ef09f6c60c38a75e8e75b13697e
Jon Sakas <jon.sakas@beatport.com>
2017-09-19 22:20:10 -0600

    Create helper functions for working with number ranges
    These will be used for working with custom sliders and knobs

---
+++ tests/utils/maths.test.js
@@ -0,0 +1,78 @@
+import { percentToPoint, pointToPercent } from '../../src/utils/maths'
+
+describe('percentToPoint', () => {
+  test('returns 127 for 1 in range [-127, 127]', () => {
+    expect(percentToPoint(-127, 127, 1)).toBe(127)
+  })
+
+  test('returns 0 for .5 in range [-127, 127]', () => {
+    expect(percentToPoint(-127, 127, .5)).toBe(0)
+  })
+
+  test('returns -127 for 0 in range [-127, 127]', () => {
+    expect(percentToPoint(-127, 127, 0)).toBe(-127)
+  })
+
+  test('returns 127 for 1 in range [0, 127]', () => {
+    expect(percentToPoint(0, 127, 1)).toBe(127)
+  })
+
+  test('returns 63.5 for .5 in range [0, 127]', () => {
+    expect(percentToPoint(0, 127, .5)).toBe(63.5)
+  })
+
+  test('returns 0 for 0 in range [0, 127]', () => {
+    expect(percentToPoint(0, 127, 0)).toBe(0)
+  })
+
+  test('returns 10 for 0 in range [10, 110]', () => {
+    expect(percentToPoint(10, 100, 0)).toBe(10)
+  })
+
+  test('returns 110 for 1 in range [10, 110]', () => {
+    expect(percentToPoint(10, 110, 1)).toBe(110)
+  })
+
+  test('returns 60 for .5 in range [10, 110]', () => {
+    expect(percentToPoint(10, 110, .5)).toBe(60)
+  })
+})
+
+describe('pointToPercent', () => {
+  test('returns 100 for 127 in range [-127, 127]', () => {
+    expect(pointToPercent(-127, 127, 127)).toBe(1)
+  })
+
+  test('returns .5 for 0 in range [-127, 127]', () => {
+    expect(pointToPercent(-127, 127, 0)).toBe(.5)
+  })
+
+  test('returns 0 for -127 in range [-127, 127]', () => {
+    expect(pointToPercent(-127, -127, -127)).toBe(0)
+  })
+
+  test('returns 100 for 127 in range [0, 127]', () => {
+    expect(pointToPercent(0, 127, 127)).toBe(1)
+  })
+
+  test('returns .5 for 63.5 in range [0, 127]', () => {
+    expect(pointToPercent(0, 127, 63.5)).toBe(.5)
+  })
+
+  test('returns 0 for 0 in range [0, 127]', () => {
+    expect(pointToPercent(0, 127, 0)).toBe(0)
+  })
+
+  test('returns 0 for 10 in range [10, 110]', () => {
+    expect(pointToPercent(10, 100, 10)).toBe(0)
+  })
+
+  test('returns 1 for 110 in range [10, 110]', () => {
+    expect(pointToPercent(10, 110, 110)).toBe(1)
+  })
+
+  test('returns .5 for 60 in range [10, 110]', () => {
+    expect(pointToPercent(10, 110, 60)).toBe(.5)
+  })
+})
+
