82e7930fd222b9e22dccd111ce821eac51fce9b7
Jon Sakas <jon.sakas@beatport.com>
2017-09-22 13:18:14 -0600

    Fix imports in Subtractor

--- src/Subtractor.js
+++ src/Subtractor.js
@@ -1,7 +1,5 @@
 import { Filter } from './Filter'
 import { Oscilloscope } from './Oscilloscope'
-import { Slider } from './components/Slider'
-import styles from './sass/section.scss'
 
 class Subtractor {
   constructor() {
