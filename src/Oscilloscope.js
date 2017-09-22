8c645787e749ac44859b13a46ea15e9d680febd5
Jon Sakas <jon.sakas@beatport.com>
2017-09-22 11:41:49 -0600

    Move drawing code out of constructor and manually bind

--- src/Oscilloscope.js
+++ src/Oscilloscope.js
@@ -3,37 +3,39 @@ class Oscilloscope {
       console.log('Oscilloscope constructed')
       this.context = context
       this.analyzer = context.createAnalyser()
-      this.draw = () => {
-        requestAnimationFrame(this.draw)
+      this.draw = this.draw.bind(this)
+    }
 
-        const canvasWidth = this.canvas.width
-        const canvasHeight = this.canvas.height
-        const canvasContext = this.canvas.getContext('2d')
+    draw() {
+      requestAnimationFrame(this.draw)
 
-        this.analyzer.getByteTimeDomainData(this.analyzerDataArray)
+      const canvasWidth = this.canvas.width
+      const canvasHeight = this.canvas.height
+      const canvasContext = this.canvas.getContext('2d')
 
-        canvasContext.fillStyle = 'rgb(200, 200, 200)'
-        canvasContext.fillRect(0, 0, canvasWidth, canvasHeight)
-        canvasContext.lineWidth = 2
-        canvasContext.strokeStyle = 'rgb(0, 0, 0)'
-        canvasContext.beginPath()
+      this.analyzer.getByteTimeDomainData(this.analyzerDataArray)
 
-        let sliceWidth = Number(canvasWidth) * 1.0 / this.analyzerBufferLength
-        let x = 0
-        for (let i = 0; i < this.analyzerBufferLength; i++) {
-            let v = this.analyzerDataArray[i] / 128.0
-            let y = v * canvasHeight/2
-            if (i === 0) {
-              canvasContext.moveTo(x, y)
-            } else {
-              canvasContext.lineTo(x, y)
-            }
-            x += sliceWidth
+      canvasContext.fillStyle = 'rgb(200, 200, 200)'
+      canvasContext.fillRect(0, 0, canvasWidth, canvasHeight)
+      canvasContext.lineWidth = 2
+      canvasContext.strokeStyle = 'rgb(0, 0, 0)'
+      canvasContext.beginPath()
+
+      let sliceWidth = Number(canvasWidth) * 1.0 / this.analyzerBufferLength
+      let x = 0
+      for (let i = 0; i < this.analyzerBufferLength; i++) {
+          let v = this.analyzerDataArray[i] / 128.0
+          let y = v * canvasHeight/2
+          if (i === 0) {
+            canvasContext.moveTo(x, y)
+          } else {
+            canvasContext.lineTo(x, y)
           }
+          x += sliceWidth
+        }
 
-        canvasContext.lineTo(canvasWidth, canvasHeight/2)
-        canvasContext.stroke()
-      }
+      canvasContext.lineTo(canvasWidth, canvasHeight/2)
+      canvasContext.stroke()
     }
 
     start(canvas) {
