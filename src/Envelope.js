4085bb681945b04c759f1db20670be74c99bc615
Jon Sakas <jon.sakas@beatport.com>
2017-10-11 23:10:32 -0600

    Update Envelope comments for developer clarity

--- src/Envelope.js
+++ src/Envelope.js
@@ -9,6 +9,7 @@ class Envelope extends Observable {
       this.audioParam = audioParam
       // the following should work, but we need to convert that scientific notation to a value somehow
       // this.maxValue = audioParam.maxValue
+      // this.minValue = audioParam.minValue
       this.maxValue = 1
       this.minValue = 0
       this._attack = 0
@@ -17,16 +18,30 @@ class Envelope extends Observable {
       this._release = 40
     }
 
+    // schedule handles the ADS of the ADSR envelope
+    //
     schedule() {
-      const sustainPercent = this._sustain * (100 / 127) * .01 // how far to drop down for the sustain
+      // how far to drop down for the sustain
+      const sustainPercent = this._sustain * (100 / 127) * .01
+
+      // reset value to 0
       this.audioParam.setValueAtTime(0, this.context.currentTime)
+
+      // ramp up to max value from attack
       this.audioParam.linearRampToValueAtTime(this.maxValue, this.context.currentTime + knobToSeconds(this._attack))
+
+      // ramp down to sustain value
       this.audioParam.linearRampToValueAtTime(this.maxValue * sustainPercent, this.context.currentTime + knobToSeconds(this._attack + this._decay))
     }
 
+    // reset handles the R of the ADSR envelope
+    //
     reset() {
+      // kill all current scheduled values and reset to current value
       this.audioParam.cancelScheduledValues(this.context.currentTime)
       this.audioParam.setValueAtTime(this.audioParam.value, this.context.currentTime)
+
+      // start decay from current value to min
       this.audioParam.linearRampToValueAtTime(this.minValue, this.context.currentTime + knobToSeconds(this._release))
     }
 
