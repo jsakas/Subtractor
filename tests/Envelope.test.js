f38a2f505f09f1e7ed40adb85206db6ab2760580
Jon Sakas <jon.sakas@beatport.com>
2017-10-14 13:49:00 -0600

    Add tests for Envelope class (WIP)

---
+++ tests/Envelope.test.js
@@ -0,0 +1,72 @@
+import 'web-audio-test-api'
+import { Envelope } from '../src/Envelope'
+
+
+describe('Envelope', () => {
+
+  describe('schedule', () => {
+
+    const audioContext = new AudioContext()
+    const gainNode = audioContext.createGain()
+    const envelope = new Envelope(audioContext, gainNode.gain)
+
+    // TODO - need to figure out how to test this function
+    test('does something', () => {
+      envelope.schedule()
+    })
+  })
+
+  describe('reset', () => {
+
+    const audioContext = new AudioContext()
+    const gainNode = audioContext.createGain()
+    const envelope = new Envelope(audioContext, gainNode.gain)
+
+    // TODO - need to figure out how to test this function
+    test('does something', () => {
+      envelope.reset()
+    })
+  })
+
+  describe('setters & getters', () => {
+
+    const audioContext = new AudioContext()
+    const gainNode = audioContext.createGain()
+    const envelope = new Envelope(audioContext, gainNode.gain)
+
+    test('attack', () => {
+      envelope.attack = 13
+      expect(envelope.attack).toEqual(13)
+    })
+
+    test('decay', () => {
+      envelope.attack = 15
+      expect(envelope.attack).toEqual(15)
+    })
+
+    test('sustain', () => {
+      envelope.attack = 17
+      expect(envelope.attack).toEqual(17)
+    })
+
+    test('release', () => {
+      envelope.attack = 19
+      expect(envelope.attack).toEqual(19)
+    })
+
+    test('amount', () => {
+      envelope.amount = 127
+      expect(envelope.amount).toEqual(127)
+    })
+
+    test('minValue', () => {
+      envelope.minValue = -3
+      expect(envelope.minValue).toEqual(-3)
+    })
+
+    test('maxValue', () => {
+      envelope.maxValue = 3
+      expect(envelope.maxValue).toEqual(3)
+    })
+  })
+})
