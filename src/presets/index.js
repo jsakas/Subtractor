75c828425c8845e6288539a79e35401362a40a8e
Jon Sakas <jon.sakas@beatport.com>
2017-10-14 20:05:33 -0600

    Update presets with new envelope settings

--- src/presets/index.js
+++ src/presets/index.js
@@ -1,6 +1,15 @@
 import Init from './Init'
 import SuperFunk from './synth/SuperFunk'
+import Plucky from './synth/Plucky'
 import DarkBass from './bass/DarkBass'
+import ElectroBass from './bass/ElectroBass'
 import ElectroFifth from './bass/ElectroFifth'
 
-export { Init, SuperFunk, DarkBass, ElectroFifth }
+export { 
+  Init, 
+  SuperFunk, 
+  Plucky, 
+  DarkBass, 
+  ElectroBass, 
+  ElectroFifth 
+}
